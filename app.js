f01abcc11aa14c5271cdedc3b8aee78fe83ceda7
Shervin <shervinmohajeri@gmail.com>
2023-08-03 15:18:35 +0100

    added round count as a user input and got all inputs from DOM

--- app.js
+++ app.js
@@ -6,3 +6,22 @@
         document.getElementById('displayTime').innerHTML = displayTime;
     }
 }
+
+// Get Elements from the DOM
+const body = document.querySelector("body");
+
+// Get inputs
+const roundsCountInput = document.getElementById("rounds");
+const roundTimeInput = document.getElementById("roundTime");
+const restTimeInput = document.getElementById("restTime");
+const startStopBtn = document.getElementById("start");
+
+// Get container for rounds and rests
+const roundsContainer = document.querySelector(".rounds-container");
+
+const startTimer = () => {
+    if (roundsCountInput.value === "" || roundTimeInput.value === "" || restTimeInput.value === "") {
+        alert("Please fill all the fields");
+        return;
+    }
+}
